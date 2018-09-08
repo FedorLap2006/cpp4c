890f3d8e790698c638fbca61f69f54437a8b9bae
FedorLap2006 <42876964+FedorLap2006@users.noreply.github.com>
2018-09-08 14:08:12 +0300

    Update vector__c.h

--- include/vector/vector__c.h
+++ include/vector/vector__c.h
@@ -1,21 +1,36 @@
 #pragma once
+#include <stdlib.h> // for malloc,calloc,free,realloc
 #include <stddef.h>
-typedef struct
+typedef struct _datastruct
 {
 	void *data;
+	struct _datastruct *next;
+	struct _datastruct *prev;
+}datastruct;
+typedef struct 
+{
+	datastruct *data_vec;
+	datastruct *top_el;
 	size_t size;
+	size_t size_el;
 	char *name;
+	
 }vector;
 /* reserve and init */
 #define init_res_vector_ptr(size,type,namevec)\ 
-	namevec->size=sizeof(type)*size;
+	namevec->size_el=sizeof(type);
+	namevec->size=size;
 	namevec->name=#namevec;
 #define init_res_vector(size,type,namevec)\ 
-	namevec.size=sizeof(type)*size;\
+	namevec.size_el=sizeof(type);\
+	namevec.size=size;
 	namevec.name=#namevec;
-///////////////////////////
-/*init(0 el)*/
-#define init_vector(type,namevec)\ 
-	namevec.size=sizeof(type);\
-	namevec.name=#namevec;
-//////////////////////////
+int push_el(vector *vec)
+{
+	
+	vec->top_el->next=((void*)0);
+	vec->top_el->next = malloc(vec->size_el);
+	if(vec->top_el->next == ((void*)0)) //...
+}
+
+void del_el(vector *vec)
