5b2beea802ddd0f7682c1c5d833cee30ecb8f52a
FedorLap2006 <42876964+FedorLap2006@users.noreply.github.com>
2018-09-08 14:37:29 +0300

    Create vector__c.c

---
+++ include/vector/vector__c.c
@@ -0,0 +1,49 @@
+#include "vector__c.h"
+
+
+/* reserve and init */
+void reserve_func(vector *vec,size_t size)
+{
+	int it;
+	int check_it;
+	//top_el=NULL;
+	if(size > 0)
+	{
+		top_el=datavec=NULL;
+		top_el = malloc(sizeof(vec->size_el));
+		if(top_el == NULL)
+		{
+			return 0;
+		}
+		
+		for(it=0;it<size;it++)
+		{
+			top_el->next=NULL;
+			top_el->next = malloc(sizeof(vec->size_el));
+			if(top_el->next == NULL)  break;
+			top_el=top_el->next;
+			
+		}
+	}
+	if(it!=size-1)
+	{
+		vec->err_mem='y';
+	}
+	vec->err_mem='n';
+	return;
+	
+}
+
+int push_el(vector *vec)
+{
+	
+	vec->top_el->next=NULL;
+	vec->top_el->next = malloc(vec->size_el);
+	if(vec->top_el->next == NULL) return 0;
+	vec->top_el=vec->top_el->next;
+	return 1;
+}
+
+void del_el(vector *vec)
+{
+}
