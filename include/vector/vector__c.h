af61de1ef41ac24955369bbff7c2e169e7ae4fc6
FedorLap2006 <42876964+FedorLap2006@users.noreply.github.com>
2018-09-08 14:35:20 +0300

    Update vector__c.h

--- include/vector/vector__c.h
+++ include/vector/vector__c.h
@@ -18,37 +18,7 @@ typedef struct
 	char err_mem;
 }vector;
 /* reserve and init */
-void reserve_func(vector *vec,size_t size)
-{
-	int it;
-	int check_it;
-	//top_el=NULL;
-	if(size > 0)
-	{
-		top_el=datavec=NULL;
-		top_el = malloc(sizeof(vec->size_el));
-		if(top_el == NULL)
-		{
-			return 0;
-		}
-		
-		for(it=0;it<size;it++)
-		{
-			top_el->next=NULL;
-			top_el->next = malloc(sizeof(vec->size_el));
-			if(top_el->next == NULL)  break;
-			top_el=top_el->next;
-			
-		}
-	}
-	if(it!=size-1)
-	{
-		vec->err_mem='y';
-	}
-	vec->err_mem='n';
-	return;
-	
-}
+void reserve_func(vector *vec,size_t size);
 
 #define init_res_vector_ptr(size,type,namevec)\
 	reserve_func(namevec,size);\
@@ -60,14 +30,6 @@ void reserve_func(vector *vec,size_t size)
 	namevec.size_el=sizeof(type);\
 	namevec.size=size;\
 	namevec.name=#namevec;
-int push_el(vector *vec)
-{
-	
-	vec->top_el->next=NULL;
-	vec->top_el->next = malloc(vec->size_el);
-	if(vec->top_el->next == NULL) return 0;
-	vec->top_el=vec->top_el->next;
-	return 1;
-}
+int push_el(vector *vec);
 
-void del_el(vector *vec)
+void del_el(vector *vec);
