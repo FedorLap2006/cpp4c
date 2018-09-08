7858a33e7a130477c5b7abc4452eeec7e1b67c86
FedorLap2006 <42876964+FedorLap2006@users.noreply.github.com>
2018-09-09 00:58:24 +0300

    Update vector__c.c
    realese delete func(iterator,vector)

--- include/vector/vector__c.c
+++ include/vector/vector__c.c
@@ -9,7 +9,8 @@ void reserve_func(vector *vec,size_t size)
 	//top_el=NULL;
 	if(size > 0)
 	{
-		top_el=datavec=NULL;
+		top_el=data_vec=NULL;
+		datavec->prev=NULL;
 		top_el = malloc(sizeof(vec->size_el));
 		if(top_el == NULL)
 		{
@@ -34,13 +35,30 @@ void reserve_func(vector *vec,size_t size)
 	
 }
 
+vector* get_el(vector *vec,int index)
+{
+	vector *cur_vec = vec;
+	//iter *ret_el;
+	
+	if(vector->size==0) return NULL;
+	
+	int it;
+	for(it=0;it<index;it++)
+	{
+		cur_vec=cur_vec->next;
+	}
+	return cur_vec;
+}
+
+
 int push_el(vector *vec)
 {
 	
 	vec->top_el->next=NULL;
 	vec->top_el->next = malloc(vec->size_el);
-	if(vec->top_el->next == NULL) return 0;
+	if(vec->top_el->next == NULL) vec->err_mem = 'y'; return 0;
 	vec->top_el=vec->top_el->next;
+	vec->err_mem='n';
 	return 1;
 }
 
@@ -57,17 +75,27 @@ void pop_el(vector *vec)
 	return;
 }
 
-vector* get_el(vector *vec,int index)
+void delete_el(iter *deleted_el,vector *vec)
 {
-	vector *cur_vec = vec;
-	//iter *ret_el;
-	
-	if(vector->size==0) return NULL;
-	
-	int it;
-	for(it=0;it<index;it++)
+	vector *prev_var,*next_var;
+	vector *saver_vector;
+	if(deleted_el->sourse!=NULL)
 	{
-		cur_vec=cur_vec->next;
+		if(deleted_el->sourse->next == NULL)
+		{
+			pop_el(vec);
+		}
+		if(deleted_el->sourse->prev == NULL)
+		{
+			saver_vector = vec->data_vec;
+			vec->data_vec=vec->data_vec->next;
+			vec->data_vec->prev=NULL;
+			if(data_vec == NULL) pop_el(vec->data_vec); saver_vector=NULL; return;
+			free(saver_vector);
+		}
+	
 	}
-	return cur_vec;
+	return;
 }
+
+
